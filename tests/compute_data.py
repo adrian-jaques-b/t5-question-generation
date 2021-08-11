8d066c3cd51b89b675f3ae67d6cc77762e51f681
asahi417 <asahi1992ushio@gmail.com>
2021-08-12 01:25:58 +0900

    fix dataset

--- tests/compute_data.py
+++ tests/compute_data.py
@@ -1,7 +1,7 @@
 from t5qg.data import get_dataset
 
 # data = 'squad'
-for data in ['tydiqa', 'squad']:
+for data in ['squad', 'tydiqa']:
     for i in ['train', 'test', 'dev']:
         get_dataset(data, split=i)
         get_dataset(data, split=i, no_prefix=True)
